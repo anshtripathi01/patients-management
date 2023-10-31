2379a7006e97efbe628c0004cd47526889d87a98
Ansh Tripathi <tripathiansh57@gmail.com>
2023-10-31 17:01:37 +0530

    fixed:calculation-issue

--- src/utils/calculateHospitalStatistics.js
+++ src/utils/calculateHospitalStatistics.js
@@ -1,6 +1,6 @@
 // Example calculateHospitalStatistics function
 export function calculateHospitalStatistics(patients, wards) {
-  if (!patients || patients.length === 0) {
+  if (!patients || patients?.length === 0) {
     return {
       totalPatients: 0,
       occupancyRate: 0,
@@ -12,20 +12,20 @@ export function calculateHospitalStatistics(patients, wards) {
     };
   }
 
-  const totalPatients = patients.length;
+  const totalPatients = patients?.length;
 
-  const occupancyRate = ((wards.length / totalPatients) * 100).toFixed(2);
+  const occupancyRate = ((wards?.length / totalPatients) * 100)?.toFixed(2);
 
-  const totalLengthOfStay = patients.reduce(
+  const totalLengthOfStay = patients?.reduce(
     (total, patient) => total + patients.length,
     0
   );
   const averageLengthOfStay = (totalLengthOfStay / totalPatients).toFixed(2);
 
   const topPerformingWard = patients.reduce((topWard = 0, patient) => {
-    if (!topWard || patient.assignedWard < topWard) {
+    if (!topWard || patient?.assignedWard < topWard) {
       return {
-        name: patient.assignedWard,
+        name: patient?.assignedWard,
         criteria: `Ward Capacity: ${
             wards.find(
                 ({ wardNumber }) =>
